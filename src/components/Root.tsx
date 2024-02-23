2610d13d2f5b662b9b6d845ad594194e6921068e
Mangpha <8350130@gmail.com>
2024-02-23 16:13:45 +0900

    Header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,26 @@
+import { Box, Button, HStack } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { FaAirbnb } from 'react-icons/fa';
 
 const Root = () => {
 	return (
-		<h1>
-			Root Page
+		<Box>
+			<HStack
+				justifyContent={'space-between'}
+				py={5}
+				px={10}
+				borderBottomWidth={1}
+			>
+				<Box color={'red.500'}>
+					<FaAirbnb size={48} />
+				</Box>
+				<HStack spacing={2}>
+					<Button>Log In</Button>
+					<Button colorScheme="red">Sign Up</Button>
+				</HStack>
+			</HStack>
 			<Outlet />
-		</h1>
+		</Box>
 	);
 };
 
