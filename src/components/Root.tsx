487079e7fc381a8772ea7222ac5bbf159160f2c3
Mangpha <8350130@gmail.com>
2024-02-23 16:42:54 +0900

    Login Modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,27 @@
-import { Box, Button, HStack } from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	HStack,
+	IconButton,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	VStack,
+	useDisclosure,
+} from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import { FaAirbnb } from 'react-icons/fa';
+import { FaAirbnb, FaLock, FaMoon, FaUser } from 'react-icons/fa';
 
 const Root = () => {
+	const { isOpen, onClose, onOpen } = useDisclosure();
+
 	return (
 		<Box>
 			<HStack
@@ -15,9 +34,48 @@ const Root = () => {
 					<FaAirbnb size={48} />
 				</Box>
 				<HStack spacing={2}>
-					<Button>Log In</Button>
+					<IconButton
+						variant="ghost"
+						aria-label="Toggle dark mode"
+						icon={<FaMoon />}
+					/>
+					<Button onClick={onOpen}>Log In</Button>
 					<Button colorScheme="red">Sign Up</Button>
 				</HStack>
+				<Modal isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>Log In</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<VStack>
+								<InputGroup>
+									<InputLeftElement
+										children={
+											<Box color={'gray.500'}>
+												<FaUser />
+											</Box>
+										}
+									/>
+									<Input variant={'filled'} placeholder="Username" />
+								</InputGroup>
+								<InputGroup>
+									<InputLeftElement
+										children={
+											<Box color={'gray.500'}>
+												<FaLock />
+											</Box>
+										}
+									/>
+									<Input variant={'filled'} placeholder="Password" />
+								</InputGroup>
+							</VStack>
+							<Button mt={4} w={'100%'} colorScheme="red">
+								Log In
+							</Button>
+						</ModalBody>
+					</ModalContent>
+				</Modal>
 			</HStack>
 			<Outlet />
 		</Box>
